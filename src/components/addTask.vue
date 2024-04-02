9fc249ed85e380c39aa099b93a431569902c73a8
Burnsedia <bburnsed1995@gmail.com>
2024-04-02 17:47:01 -0400

    relearning Vuejs after working with React for years

---
+++ src/components/addTask.vue
@@ -0,0 +1,25 @@
+// create a new task
+<template>
+  <div >
+    <h1>Add Task</h1>   
+    <input type="text" v-model="newTask" @click="addTask">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addTask' 
+  date: () => {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask() {
+      this.$emit('add-task', this.newTask)
+      this.newTask = ''
+    }
+  } 
+
+}
+</script>

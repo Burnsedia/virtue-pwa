767113f90b31823a0deca9900f37dbbd24cb1dbb
Burnsedia <bburnsed1995@gmail.com>
2025-06-27 23:29:53 -0400

    got basic crud for api done,just need permissions

--- backend/core/api/views.py
+++ backend/core/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Organization, Project, Issue, TimeLog
+from .serializers import *
 
-# Create your views here.
+class OrganizationViewSet(viewsets.ModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class IssueViewSet(viewsets.ModelViewSet):
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
+
+class TimeLogViewSet(viewsets.ModelViewSet):
+    queryset = TimeLog.objects.all()
+    serializer_class = TimeLogSerializer
+

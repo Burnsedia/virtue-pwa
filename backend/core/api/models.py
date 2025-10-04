9e0b9cf8b66ffd8fae1130a4a0b0a13b9d115b5a
Burnsedia <bburnsed1995@gmail.com>
2025-10-04 19:49:52 -0400

    feat: add Profile model and signals

--- backend/core/api/models.py
+++ backend/core/api/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_client = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)

6c9a8ed99ff9f1762d52c7c66253218c44abce65
Burnsedia <bburnsed1995@gmail.com>
2025-12-05 19:33:08 -0500

    refactor(djoser_serializers): removed unneeded

--- backend/core/api/djoser_serializers.py
+++
@@ -1,8 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-from rest_framework import serializers
-
-class UserCreateSerializer(UserCreateSerializer):
-    username = serializers.CharField(required=False) # Explicitly define username
-
-    class Meta(UserCreateSerializer.Meta):
-        fields = ('id', 'email', 'username', 'password')
